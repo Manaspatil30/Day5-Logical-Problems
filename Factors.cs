726df6c4c0c4533f62dbe4484fabe6077eaa2dd4
unknown <manaspatil2030@gmail.com>
2021-09-16 00:46:21 +0530

    Finding Factors program added

---
+++ Factors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_5
+{
+    class Factors
+    {
+        public static void GetFactors()
+        {
+            int num;
+            int x;
+
+            Console.WriteLine("Enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The factors of the entered number are :");
+
+            for (x = 1; x <= num; x++)
+            {
+                if (num % x == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+
+
+        }
+    }
+}
