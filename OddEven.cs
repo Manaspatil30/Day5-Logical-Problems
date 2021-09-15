392de87e9830552ef9bbea99368b28fd58510462
unknown <manaspatil2030@gmail.com>
2021-09-16 00:49:42 +0530

    Odd even program added

---
+++ OddEven.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_5
+{
+    class OddEven
+    {
+        public static void getOddEven()
+        {
+            int i;
+            Console.Write("Enter a Number : ");
+            i = Convert.ToInt32(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+            }
+        }
+    }
+}
