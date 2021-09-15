810e0bf1d3f47a0db5c85d32e8f4c63c30db9da8
unknown <manaspatil2030@gmail.com>
2021-09-16 00:39:44 +0530

    Main program file added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoin.flipCoinPersentage();
+            //LeapYear.GetLeapYr();
+            //PowerOf2.GetPower();
+            //Harmonic.GetHarmonicNum();
+            //Factors.GetFactors();
+            //QuotientAndRemainder.QuesRem();
+            //swapNum.swap();
+            //OddEven.getOddEven();
+            //VowelConsonant.getVowCon();
+            //LargestNum.getLargetNum();
+            Console.ReadKey();
+        }
+    }
+}
