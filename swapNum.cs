cbc5865dc297ab3310c1280c07816cff03f1b118
unknown <manaspatil2030@gmail.com>
2021-09-16 00:48:56 +0530

    Swap numbers program added

---
+++ swapNum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_5
+{
+    class swapNum
+    {
+        public static void swap()
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("Enter value of a");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter value of b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a * b; //a=50 (5*10)      
+            b = a / b; //b=5 (50/10)      
+            a = a / b; //a=10 (50/5)    
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
